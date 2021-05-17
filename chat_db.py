fbd5e502d4783c91d7ee7ec504e1c954bb1d707a
Vatsal Bhuva <vatsalbhuva11@gmail.com>
2021-05-17 12:24:41 +0300

    join room working thru unique host, session

--- chat_db.py
+++ chat_db.py
@@ -7,8 +7,31 @@ db = sql.connect(
         database = 'roomdb'
     )
 
-def createTable(host):
+def createTable(host,roomID):
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS t_{name} (message varchar(150) default 'None')".format(name = host))
+    cursor.execute('''CREATE TABLE IF NOT EXISTS t_{name}
+     (roomID varchar(10) default 'None',
+      message varchar(150) default 'None')'''.format(name = host))
+    cursor.execute('''INSERT INTO t_{name}(roomID) values ({id})'''.format(name = host, id = roomID))
     db.commit()
+    
+def connect(host, roomID):
+    cursor = db.cursor()
+    cursor.execute(''' SHOW TABLES LIKE 't_{host}' '''.format(host = host))
+    hostname = cursor.fetchone()
+
+    if hostname == None:
+        return "This user has not hosted a room currently."
+    else:
+        hostname = hostname[0]
+        cursor.execute(" SELECT roomID from t_{host} where roomID != 'None' ".format(host = host))
+        checkID = cursor.fetchone()[0]
+
+        if checkID == roomID and hostname[2::] == host:
+            return "Successfully connected to the chatroom!"
+        else:
+            return "One or more details are incorrectly entered."
+
+
+
     
