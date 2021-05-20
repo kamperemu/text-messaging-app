93402214721540bc00973d705685f1e33db772d9
Vatsal Bhuva <vatsalbhuva11@gmail.com>
2021-05-20 09:54:02 +0300

    made necessary comments

--- chat_db.py
+++ chat_db.py
@@ -7,6 +7,8 @@ db = sql.connect(
         database = 'roomdb'
     )
 
+#so each table represents a chat room. each unique chatroom has a unique host and a unique session id.
+#named the table by the user's username for easier referencing
 def createTable(host,roomID):
     cursor = db.cursor()
     cursor.execute(''' SHOW TABLES LIKE 't_{host}' '''.format(host = host))
@@ -23,6 +25,7 @@ def createTable(host,roomID):
     
 def connect(curUser,host,roomID):
     cursor = db.cursor()
+    #get list of all tables to see if the host has already hosted one room or not
     cursor.execute(''' SHOW TABLES LIKE 't_{host}' '''.format(host = host))
     hostname = cursor.fetchone()
 
@@ -33,19 +36,30 @@ def connect(curUser,host,roomID):
         cursor.execute(" SELECT roomID from t_{host} where roomID != 'None' ".format(host = host))
         checkID = cursor.fetchone()[0]
 
+        #successfully connected to a room, insert the user's name in the table for the corresponding host.
         if checkID == roomID and hostname[2::] == host:
             cursor.execute("INSERT INTO t_{host}(users) VALUES ('{user}')".format(host = host, user = curUser))
             return "Successfully connected to the chatroom!"
         else:
             return "One or more details are incorrectly entered."
 
+#get list of all users in a room
 def joinedUsers(host):
     cursor = db.cursor()
     cursor.execute("SELECT users from t_{host}".format(host = host))
     data = cursor.fetchall()
     return data
 
+def appendMsg(user, msg, host):
+    cursor = db.cursor()
+    cursor.execute('''INSERT INTO t_{host}(users, message)
+                    values ('{user}','{msg}')'''.format(host = host, user = user, msg = msg))
+    db.commit()
 
-
+def get_allMsg(host):
+    cursor = db.cursor()
+    cursor.execute("SELECT users,message from t_{host}".format(host = host))
+    data = cursor.fetchall()
+    return data
 
     
